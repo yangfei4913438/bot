2ce5e73e11e767ec7392c75fc01b08495f8d01b7
杨飞 <yangfei86@vip.qq.com>
2024-05-05 13:49:14 +0800

    init

---
+++ oss.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import oss2
+from oss2.credentials import EnvironmentVariableCredentialsProvider
+
+# 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量 OSS_ACCESS_KEY_ID 和 OSS_ACCESS_KEY_SECRET。
+auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+
+# 填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+# yourBucketName填写存储空间名称。
+bucket = oss2.Bucket(
+    auth=auth,
+    endpoint='https://oss-cn-shanghai.aliyuncs.com',
+    bucket_name='yangfei-wiki'
+)
+
+def audio_exists(target_dir:str, filename: str):
+    """检查音频是否存在"""
+    # 填写Object的完整路径，Object完整路径中不能包含Bucket名称。
+    return bucket.object_exists(f'{target_dir}/{filename}')
+
+
+def audio_download(target_dir:str, filename: str):
+    """ 下载音频 """
+    # 填写Object的完整路径，Object完整路径中不能包含Bucket名称。
+    file = bucket.get_object(f'{target_dir}/{filename}')
+    # 返回文件内容
+    return file.read()
+
+def audio_del(target_dir:str, filename: str):
+    """ 删除音频 """
+    # 填写Object的完整路径，Object完整路径中不能包含Bucket名称。
+    return bucket.delete_object(f'{target_dir}/{filename}')
---
+++ server.py
@@ -0,0 +1,68 @@
+import telebot
+import urllib.parse
+import requests
+import os
+import asyncio
+from dotenv import load_dotenv
+
+from oss import audio_download, audio_exists, audio_del
+
+# 加载环境变量配置文件
+load_dotenv()
+
+bot_api = os.getenv("BOT_API")
+ai_server = os.getenv("AI_SERVER")
+
+bot_instance = telebot.TeleBot(token=bot_api)
+
+
+@bot_instance.message_handler(commands=['start'])
+def send_welcome(message):
+    print("发送消息:", message.text)
+    # 直接回复
+    bot_instance.send_message(message.chat.id, "你好! 我是周半仙，有什么可以帮助你的吗？")
+
+
+@bot_instance.message_handler(func=lambda message: True)
+def echo_all(message):
+    try:
+        print("发送消息:", message.text)
+        encode_text = urllib.parse.quote(message.text)
+        base_url = ai_server
+        response = requests.post(
+            url=f'{base_url}/chat?user_id={message.chat.id}&query={encode_text}',
+            timeout=100
+        )
+
+        if response.status_code == 200:
+            print("返回数据:", response.json())
+            data = response.json()
+            if "msg" in data:
+                bot_instance.reply_to(message, data["msg"].encode('utf-8'))
+                asyncio.run(check_audio(message, 'audio', f"{data["id"]}.mp3"))
+            else:
+                bot_instance.reply_to(message, "对不起，我不知道怎么回复你")
+
+    except requests.RequestException as e:
+        print("发送消息出错:", e)
+        bot_instance.reply_to(message, "对不起，我不知道怎么回复你")
+
+
+async def check_audio(message: any, target_dir:str, filename: str):
+    """ 检查音频是否存在并发送 """
+    while True:
+        # 检查音频是否存在
+        if audio_exists(target_dir, filename):
+            # 获取音频
+            file = audio_download(target_dir, filename)
+            # 发送音频
+            bot_instance.send_audio(message.chat.id, file)
+            # 删除音频, 释放云端空间
+            audio_del(target_dir, filename)
+            break
+        else:
+            print("waiting")
+            await asyncio.sleep(1)  # 使用asyncio.sleep(1)来等待1秒
+
+# 启动服务
+bot_instance.infinity_polling()
